8f7143462d1d57593432a9a1dbeb47d47351299c
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-06-23 11:23:50 +0900

    add team models and fix init_data

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 from fastapi import FastAPI, HTTPException, Query, Depends
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import Field, Session, SQLModel, create_engine, select, Relationship
+
+
+class TeamBase(SQLModel):
+    name: str = Field(index=True)
+    headquarters: str
+
+
+class Team(TeamBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+    heroes: list["Hero"] = Relationship(back_populates="team")
+
+
+class TeamCreate(TeamBase):
+    pass
+
+
+class TeamPublic(TeamBase):
+    id: int
+
+
+class TeamUpdate(SQLModel):
+    name: str | None = None
+    headquarters: str | None = None
 
 
 class HeroBase(SQLModel):
@@ -7,10 +31,14 @@ class HeroBase(SQLModel):
     secret_name: str
     age: int | None = Field(default=None, index=True)
 
+    team_id: int | None = Field(default=None, foreign_key="team.id")
+
 
 class Hero(HeroBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
     hashed_password: str = Field()
+
+    team: "Team" = Relationship(back_populates="heroes")
 
 
 class HeroCreate(HeroBase):
@@ -26,6 +54,7 @@ class HeroUpdate(SQLModel):
     secret_name: str | None = None
     age: int | None = None
     password: str | None = None
+    team_id: int | None = None
 
 
 sqlite_file_name = "database.db"
@@ -33,6 +62,11 @@ sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, echo=False, connect_args=connect_args)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
 
 
 def create_db_and_tables():
@@ -82,18 +116,18 @@ def init_data():
     ]
 
     for hero in heroes:
-        created = create_hero(hero=hero)
-        print(created)
+        with Session(engine) as session:
+            hashed_password = hash_password(hero.password)
+            extra_data = {"hashed_password": hashed_password}
+            db_hero = Hero.model_validate(hero, update=extra_data)
+            session.add(db_hero)
+            session.commit()
+            session.refresh(db_hero)
 
 
 def hash_password(password: str) -> str:
     # Use something like passlib here
     return f"not really hashed {password} hehehe"
-
-
-def get_session():
-    with Session(engine) as session:
-        yield session
 
 
 app = FastAPI()
@@ -102,7 +136,64 @@ app = FastAPI()
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
-    # init_data()
+    init_data()
+
+
+@app.post("/teams/", response_model=TeamPublic)
+def create_team(*, session: Session = Depends(get_session), team: TeamCreate):
+    db_team = Team.model_validate(team)
+    session.add(db_team)
+    session.commit()
+    session.refresh(db_team)
+    return db_team
+
+
+@app.get("/teams/", response_model=list[TeamPublic])
+def read_teams(
+    *,
+    session: Session = Depends(get_session),
+    offset: int = 0,
+    limit: int = Query(default=100, le=100),
+):
+    teams = session.exec(select(Team).offset(offset).limit(limit)).all()
+    return teams
+
+
+@app.get("/teams/{team_id}", response_model=TeamPublic)
+def read_team(*, session: Session = Depends(get_session), team_id: int):
+    team = session.get(Team, team_id)
+    if not team:
+        raise HTTPException(status_code=404, detail="Team not found")
+    return team
+
+
+@app.patch("/teams/{team_id}", response_model=TeamPublic)
+def update_team(
+    *,
+    session: Session = Depends(get_session),
+    team_id: int,
+    team: TeamUpdate,
+):
+    db_team = session.get(Hero, team_id)
+    if not team:
+        raise HTTPException(status_code=404, detail="Team not found")
+    team_data = team.model_dump(exclude_unset=True)
+    for key, value in team_data.items():
+        setattr(db_team, key, value)
+    session.add(db_team)
+    session.commit()
+    session.refresh(db_team)
+    return db_team
+
+
+@app.delete("/teams/{team_id}")
+def delete_team(*, session: Session = Depends(get_session), team_id: int):
+    team = session.get(Team, team_id)
+    if not team:
+        raise HTTPException(status_code=404, detail="Team not found")
+    session.delete(team)
+    session.commit()
+    return {"ok": True}
 
 
 @app.post("/heroes/", response_model=HeroPublic)
